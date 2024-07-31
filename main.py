2e2d74f1c4081d95e89e29a89f4b482b637aeadc
Julian Mendez <163798250+julian73KMG@users.noreply.github.com>
2024-07-31 14:59:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,10 @@ def main():
     u = [1,2,3]
     v = [4,5,6]
     w = [1/2,7/5,3/2]
-
     
     print(ejercicios.promedio_arreglo_r(w))
     print(ejercicios.producto_punto_r(u,v))
     print(ejercicios.producto_directo_r(w,v))
-
-    return 0
 
 if __name__=='__main__':
     main()
